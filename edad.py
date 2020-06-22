eea66d56bbc1524ab1b912b2eb7f1aaf91d20daf
Jefry Navas <jnavass@est.ups.edu.ec>
2020-06-22 12:20:03 -0500

    Commit inicial

---
+++ edad.py
@@ -0,0 +1,16 @@
+# Codigo de ejemplo para Git
+# Autor: Jefry Navas <jnavass@est.ups.edu.ec>
+# Fecha: 22-06-2020
+# Versión: 0.0.1
+#
+# Calcula tu edad al año actual
+#
+
+anio_actual = 2020
+nombre = input("Cuál es tu nombre: ")
+anio_nacimiento = int(input("En que año naciste: "))
+
+edad = anio_actual - anio_nacimiento
+
+print("\nHola ",nombre," tu edad en este año será: ",edad);
+
